0c7d4dae1cc57a24c65092b9e63cbde68914e2ce
Brian Kaufman <bpkaufman4@gmail.com>
2021-09-08 16:22:31 -0500

    created routes to update, delete, and order posts

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(express.urlencoded({ extended: true}));
 
 app.use(routes);
 
+// turn on connection to database and server
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
